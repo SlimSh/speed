1e3d366f9740df63da62dd5ad0dfcc983ca49dcc
ignatovD <ignatov@jivosite.com>
2021-07-30 07:05:27 +0300

    Show what

--- src/components/Delivery/index.tsx
+++ src/components/Delivery/index.tsx
@@ -13,7 +13,8 @@ interface IDelivery {
 }
 
 interface IProps {
-  deliverys: { node: IDelivery }[];
+  deliverys: {
+    node: IDelivery }[];
 }
 
 export default function index({ deliverys }: IProps) {
