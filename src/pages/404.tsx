df25b53cb01af5486c5c081b4e80caaf6960eca0
ignatovD <ignatov@jivosite.com>
2021-07-30 07:36:17 +0300

    Hook must work

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import LocationService from '../utils/locations';
 
 
-const NotFoundPage = ({location}: any) => {
+const NotFoundPage = ({ location }: any) => {
   LocationService.location = location;
   return (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
-)}
+    <div>
+      <h1>NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </div>
+  )
+}
 
 export default NotFoundPage
